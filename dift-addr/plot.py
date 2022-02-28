724a55e1b41741c38ea00021645d23775cea6aa2
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2022-02-28 11:11:21 +0100

    Update plotting script

--- dift-addr/plot.py
+++ dift-addr/plot.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 
-# dift-addr --- Dynamic Information Flow Tracking on memory ADDResses
 # Copyright (C) 2022  Xiaoyue Chen
 
 # This file is part of dift-addr.
 
-# This program is free software: you can redistribute it and/or modify
+# dift-addr is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
+# dift-addr is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with dift-addr.  If not, see <http://www.gnu.org/licenses/>.
 
 import pandas as pd
 import sys
@@ -25,6 +24,10 @@ from matplotlib import pyplot as plt
 df = pd.read_csv(sys.stdin)
 
 print(df.head())
+
+if len(sys.argv) > 1:
+    plt.title(sys.argv[1])
+
 plt.plot(df.executed, df.addr_mem, label='contains memory address')
 plt.plot(df.executed, df.addr_any, label='any seen address')
 plt.xlabel('#ins executed')
